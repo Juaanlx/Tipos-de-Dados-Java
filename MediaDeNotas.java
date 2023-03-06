4900bb8ecba161d0295c7644b7398bd9b23f7072
Juaanlx <juan.manobrown@hotmail.com>
2023-03-06 13:42:10 +0000

    aula_0603

---
+++ MediaDeNotas.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MediaDeNotas {
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner (System.in);
+        int n1, n2, media;
+
+        System.out.println("Informe o valor da primeira nota: ");
+        n1 = input.nextInt();
+
+        System.out.println("Informe o valor da segunda nota: ");
+        n2 = input.nextInt();
+    }
+    
+}
